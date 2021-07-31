15bd0bc551dc176fb839a7adc0f7ba1796ea23ee
marceloruilova <marcelo.rui11@gmail.com>
2021-07-31 12:29:18 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import SearchBar from "./components/SearchBarComponent";
 
 function App() {
+  const headerTable = [
+    "Username",
+    "First Name",
+    "Last Name",
+    "Email",
+    "Location",
+    "...",
+  ];
+  const aux = { username: "Jua", email: "jaklsdf@hotmail.com" };
+  const [datos, setDatos] = useState([]);
+  const [input, setInput] = useState("");
+  const [postListDefault, setPostListDefault] = useState([]);
+
+  useEffect(() => {
+    let fetch = async () => {
+      const posts = await axios.get(
+        "https://randomuser.me/api/?page=1&results=10&seed=abc"
+      );
+      setDatos(posts.data.results);
+      setPostListDefault([aux]);
+    };
+    fetch();
+  }, []);
+  const updateInput = async (input) => {
+    const filtered = postListDefault.filter((cancion) => {
+      return cancion.username.toLowerCase().includes(input.toLowerCase());
+    });
+    setInput(input);
+    setDatos(filtered);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-box-container ">
+      <SearchBar input={input} onChange={updateInput} />
+      {headerTable.map((item) => (
+        <div>
+          <div className="calendar" key="item">
+            {item}
+          </div>
+          {headerTable.map((hour) => {
+            for (let index = 0; index < 15; index++) {
+              //Aqui utilizaría la data del api pero no me deja hacer el map, capaz usaría un for, seguiré avanzando
+              switch (item) {
+                case "Username":
+                  return (
+                    <div className="calendar" key="hour">
+                      {aux.username}
+                    </div>
+                  );
+                case "First Name":
+                  return (
+                    <div className="calendar" key="hour">
+                      alvarez
+                    </div>
+                  );
+                case "Last Name":
+                  return (
+                    <div className="calendar" key="hour">
+                      juan
+                    </div>
+                  );
+                case "Email":
+                  return (
+                    <div className="calendar" key="hour">
+                      alvarez
+                    </div>
+                  );
+                case "Location":
+                  return (
+                    <div className="calendar" key="hour">
+                      alvarez
+                    </div>
+                  );
+                case "...":
+                  return (
+                    <div className="calendar" key="hour">
+                      ...
+                    </div>
+                  );
+                default:
+                  break;
+              }
+            }
+          })}
+        </div>
+      ))}
     </div>
   );
 }
